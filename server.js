90b7c743546b17e67cfff16941cef801a34ae8ae
Salawu O. Joseph <sojbaba98@gmail.com>
2024-11-30 13:26:09 +0100

    First goal achieved

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5500" }));
+app.use(cors({ origin: "*" }));
 
 
 // Swagger Setup
